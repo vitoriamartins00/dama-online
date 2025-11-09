f27b1cb39a56e1587dcd242e130c15da85506b7b
Vitoria Martins <martinsvitoria47@gmail.com>
2025-11-09 20:19:42 -0300

    cadastro-login-logout-autenticacao

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () =>{
         // acessando a variavel .env (MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI);
         
-        console.log(`MongoDB conectado: ${cnn.connection.host}`)
+        console.log(`MongoDB conectado: ${conn.connection.host}`)   
     } catch (err) {
         console.error('Erro na conexao com MongoDB:', err.message);
         process.exit(1);
--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 require('dotenv').config();     //carrega o env
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/serverConfig');
 const connectDB = require('./config/db');
+const user = require('./models/user'); 
+const authRoutes = require('./routes/authRoutes');
+const protect = require('./middleware/authmiddle');
 
-const app = express ();
+const app = express ();  
 
+// estabelece a conexao com bd
+connectDB();
+
+app.use(bodyParser.json()); //cnoverte o corpo da requisicao (req.body) em um obj json
+app.use(cookieParser());    //permite que o servidor acesse e manipule cookies
+
+app.use(express.static('public'));  
+
+//conectando - rota de autenticacao (cadastro, login...) 
+app.use('/api/auth', authRoutes);
+
+//redireciona o usuario para a pag de login
 app.get('/', (req,res) =>{
-    res.send('teste de corno')
+    res.redirect('/login.html');
+});
+
+//rota de teste (#excluir depois)
+app.get('/api/dados/protegidos', protect, (req, res) =>{
+    res.json({
+        message: `Autenticado, ${req.userId} `,
+        data: 'Seus dados confidenciais'
+    });
 });
 
 app.listen(config.PORT, () =>{
